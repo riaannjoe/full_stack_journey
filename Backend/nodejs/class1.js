216f72d2d55bff8f70de7e329de974aa2a39cf54
riaannjoe <riaannjoeai@gmail.com>
2026-06-18 00:42:08 +0530

    commit

---
+++ Backend/nodejs/class1.js
@@ -0,0 +1,11 @@
+//nodeJS is an environment to run JS outside the browser
+
+
+const amount = 12;
+
+if(amount < 10){
+    console.log('small number');
+}
+else{
+    console.log('large number');
+}
